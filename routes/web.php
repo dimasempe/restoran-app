dd96c4fbdd2a49da3040fd2f86f46cc15d34257e
Dimas Maulana Putra <dimasmaulanaputra@MacBook-Air-Dimas.local>
2024-10-25 10:15:27 +0700

    untuk production

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Models\Order;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
-    $order = Order::find(2);
-    return $order->sumOrderPrice();
+    return view('welcome');
+    // $order = Order::find(2);
+    // return $order->sumOrderPrice();
 });
